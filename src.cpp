11d9c2754523861375cc017367dd88838236115d
moonyaeyoon <moonyaeyoon@naver.com>
2024-07-31 19:42:05 +0900

    feat : 필터 함수 생성

--- src.cpp
+++ src.cpp
@@ -6,6 +6,184 @@
 
 typedef uint8_t BYTE;
 
+// 평균값 필터
+void AverageConv(BYTE* Img, BYTE* Out, int W, int H) {
+    double Kernel[3][3] = {
+        {0.11111, 0.11111, 0.11111},
+        {0.11111, 0.11111, 0.11111},
+        {0.11111, 0.11111, 0.11111}
+    };
+    double SumProduct = 0.0;
+
+    for (int i = 1; i < H - 1; i++) {
+        for (int j = 1; j < W - 1; j++) {
+            for (int m = -1; m <= 1; m++) {
+                for (int n = -1; n <= 1; n++) {
+                    SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+                }
+            }
+            Out[i * W + j] = (BYTE)SumProduct;
+            SumProduct = 0.0;
+        }
+    }
+}
+
+// 가우시안 평균값 필터
+void GaussAvrConv(BYTE* Img, BYTE* Out, int W, int H) {
+    double Kernel[3][3] = {
+        {0.0625, 0.125, 0.0625},
+        {0.125, 0.25, 0.125},
+        {0.0625, 0.125, 0.0625}
+    };
+    double SumProduct = 0.0;
+
+    for (int i = 1; i < H - 1; i++) {
+        for (int j = 1; j < W - 1; j++) {
+            for (int m = -1; m <= 1; m++) {
+                for (int n = -1; n <= 1; n++) {
+                    SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+                }
+            }
+            Out[i * W + j] = (BYTE)SumProduct;
+            SumProduct = 0.0;
+        }
+    }
+}
+
+// Prewitt 필터 (X 방향)
+void Prewitt_X_Conv(BYTE* Img, BYTE* Out, int W, int H) {
+    double Kernel[3][3] = {
+        {-1.0, 0.0, 1.0},
+        {-1.0, 0.0, 1.0},
+        {-1.0, 0.0, 1.0}
+    };
+    double SumProduct = 0.0;
+
+    for (int i = 1; i < H - 1; i++) {
+        for (int j = 1; j < W - 1; j++) {
+            for (int m = -1; m <= 1; m++) {
+                for (int n = -1; n <= 1; n++) {
+                    SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+                }
+            }
+            Out[i * W + j] = abs((long)SumProduct) / 3;
+            SumProduct = 0.0;
+        }
+    }
+}
+
+// Prewitt 필터 (Y 방향)
+void Prewitt_Y_Conv(BYTE* Img, BYTE* Out, int W, int H) {
+    double Kernel[3][3] = {
+        {-1.0, -1.0, -1.0},
+        {0.0, 0.0, 0.0},
+        {1.0, 1.0, 1.0}
+    };
+    double SumProduct = 0.0;
+
+    for (int i = 1; i < H - 1; i++) {
+        for (int j = 1; j < W - 1; j++) {
+            for (int m = -1; m <= 1; m++) {
+                for (int n = -1; n <= 1; n++) {
+                    SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+                }
+            }
+            Out[i * W + j] = abs((long)SumProduct) / 3;
+            SumProduct = 0.0;
+        }
+    }
+}
+
+// Sobel 필터 (X 방향)
+void Sobel_X_Conv(BYTE* Img, BYTE* Out, int W, int H) {
+    double Kernel[3][3] = {
+        {-1.0, 0.0, 1.0},
+        {-2.0, 0.0, 2.0},
+        {-1.0, 0.0, 1.0}
+    };
+    double SumProduct = 0.0;
+
+    for (int i = 1; i < H - 1; i++) {
+        for (int j = 1; j < W - 1; j++) {
+            for (int m = -1; m <= 1; m++) {
+                for (int n = -1; n <= 1; n++) {
+                    SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+                }
+            }
+            Out[i * W + j] = abs((long)SumProduct) / 4;
+            SumProduct = 0.0;
+        }
+    }
+}
+
+// Sobel 필터 (Y 방향)
+void Sobel_Y_Conv(BYTE* Img, BYTE* Out, int W, int H) {
+    double Kernel[3][3] = {
+        {-1.0, -2.0, -1.0},
+        {0.0, 0.0, 0.0},
+        {1.0, 2.0, 1.0}
+    };
+    double SumProduct = 0.0;
+
+    for (int i = 1; i < H - 1; i++) {
+        for (int j = 1; j < W - 1; j++) {
+            for (int m = -1; m <= 1; m++) {
+                for (int n = -1; n <= 1; n++) {
+                    SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+                }
+            }
+            Out[i * W + j] = abs((long)SumProduct) / 4;
+            SumProduct = 0.0;
+        }
+    }
+}
+
+// 라플라스 필터
+void Laplace_Conv(BYTE* Img, BYTE* Out, int W, int H) {
+    double Kernel[3][3] = {
+        {-1.0, -1.0, -1.0},
+        {-1.0, 8.0, -1.0},
+        {-1.0, -1.0, -1.0}
+    };
+    double SumProduct = 0.0;
+
+    for (int i = 1; i < H - 1; i++) {
+        for (int j = 1; j < W - 1; j++) {
+            for (int m = -1; m <= 1; m++) {
+                for (int n = -1; n <= 1; n++) {
+                    SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+                }
+            }
+            Out[i * W + j] = abs((long)SumProduct) / 8;
+            SumProduct = 0.0;
+        }
+    }
+}
+
+// 라플라스 필터 (동적 범위 조정)
+void Laplace_Conv_DC(BYTE* Img, BYTE* Out, int W, int H) {
+    double Kernel[3][3] = {
+        {-1.0, -1.0, -1.0},
+        {-1.0, 9.0, -1.0},
+        {-1.0, -1.0, -1.0}
+    };
+    double SumProduct = 0.0;
+
+    for (int i = 1; i < H - 1; i++) {
+        for (int j = 1; j < W - 1; j++) {
+            for (int m = -1; m <= 1; m++) {
+                for (int n = -1; n <= 1; n++) {
+                    SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+                }
+            }
+            Out[i * W + j] = (SumProduct > 255.0) ? 255 : (SumProduct < 0.0) ? 0 : (BYTE)SumProduct;
+            SumProduct = 0.0;
+        }
+    }
+}
+
+
+
 // SaveBMPFile
 void SaveBMPFile(BITMAPFILEHEADER hf, BITMAPINFOHEADER hInfo,
                  RGBQUAD* hRGB, BYTE* Output, int W, int H, const char* FileName)
